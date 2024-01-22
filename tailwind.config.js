a814e01dc004dfa7f0884a3792fab16c58d1dd35
Jason Yang <Jasonyang602@gmail.com>
2024-01-22 06:26:52 -0500

    polishing game over screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,16 @@ module.exports = {
     extend: {
       backgroundImage: {
         bluepaint: "url('/src/images/Vector(1).png')",
-        mesh: "url('/src/images/meshes.png')",
+        mesh: "url('/src/images/Group 18(4).png')",
         nund: "url('/src/images/fillSketch.png')",
         arrow: "url('/src/images/arrow.png')",
         gb: "url('/src/images/GB.png')",
         home: "url('/src/images/home-sketch 1.png')",
         linescore: "url('/src/images/linescore.png')",
-        gamebrush: "url('/src/images/Group 16(1).png')"
+        gamebrush: "url('/src/images/Group 16(1).png')",
+        gameoverbrush: "url('/src/images/Group 17(1).png')",
+        submitbutton: "url('/src/images/Group 12.svg')",
+        namebox: "url('/src/images/Rectangle 14(1).png')"
       },
       colors: {
         gold: "#FFD700",
@@ -27,6 +30,7 @@ module.exports = {
         ldbd: "#86CCFF",
         score: "#A7FF7D",
         gameovercont: "#395698",
+        lb: "#85CCFF", 
       },
     },
   },
